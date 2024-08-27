8e2f5a0f5c11d3b78c6f73cfe986000698a0bc97
Deep95y <deeprkumar1995@gmail.com>
2024-08-27 19:38:27 +0530

    css fixed

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -5,21 +5,15 @@ let reviews = [
 		name: 'Company: CUVETTE TECH PVT LTD, Banglore',
 		position: 'position: Full Stack Web Developer Intern',
 		image: images.avatar0,
-		review: `During my internship at Cuvette tech (Nov 2023 - June 2024) Developed an e-commerce site for purchasing educational courses. Implemented cource listing, user authentication, user permissions and shopping cart features along with search auser perissions APIs.Designed and Implemented features updating cource inventory, Generating sales report for admin panel in tabular form.Developed Apis and managed version control with Git, Gitlab`
+		review: `During my internship at Cuvette Tech (November 2023 - June 2024), I contributed to an e-commerce platform for purchasing educational courses. I was responsible for implementing key features such as course listings, user authentication, and permissions, as well as a shopping cart system. I designed and built APIs for searching and managing user permissions. I also developed routes for generating sales reports in a tabular format for the admin panel. Throughout the project, I was motivated to explore and expand my skillset.`
 	},
 	{
-		name: 'Company: GURUKUL COACHING iNSTITUTE',
-		position: 'position: TECHNICAL CONTENT DEVELOPER',
+		name: 'Company: GURUKUL COACHING iNSTITUTE', 
+		position: 'position: Academic Tutor',
 		image: images.avatar1,
-		review: `During my Job duration (Oct 2023 - Mar 2023) Develop cource content that matches the institutes curriculum and educational standards.Delivering presentations ad demonstrations.Designing assessment and quizzes to evaluate student progress.`
+		review: `During my Job duration (Oct 2020 - Mar 2023), I worked with students of multiple grades to make challenging subjects easier to understand. I customized my teaching to their needs, tracked their progress with assessments, and shared study strategies to help them do well in their exams.`
 	},
 
-	{
-		name: 'Company: SERVIZ PVT LTD (Remote)',
-		position: 'position: Part-time Website Developer',
-		image: images.avatar2,
-		review: `As a part-time Web Developer at RajasthanIT Center, a franchise of NComputing (Jan 2021 - Jun 2021), I designed and developed a modern multi-page landing website to showcase their product line. I deployed the website, optimized it for search engines, and contributed to their online success. Skills: Bootstrap, Web Hosting,godaddy, cloudflare, Hostinger, HTML5, CSS, JavaScript.`,
-	},
 	
 ];
 
